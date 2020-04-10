9ac3db9c482d04dc5c49c92cd6c7215728ae4ed7
vitor7001 <vitorgomes7001@gmail.com>
2020-04-09 21:07:58 -0300

    Inicio do TDD com o primeiro teste falhando simulando o post para a rota

---
+++ src/test/java/com/vitor/libraryapi/api/resource/BookControllerTest.java
@@ -0,0 +1,51 @@
+package com.vitor.libraryapi.api.resource;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@WebMvcTest
+@AutoConfigureMockMvc
+public class BookControllerTest {
+
+	static String BOOK_API = "/api/books";
+
+	@Autowired
+	MockMvc mvc;
+
+	@Test
+	@DisplayName("Deve criar um livro com sucesso.")
+	public void createBookTest() throws Exception {
+
+		String json = new ObjectMapper().writeValueAsString(null);
+
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post(BOOK_API)
+				.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON).content(json);
+
+		mvc.perform(request).andExpect(status().isCreated()).andExpect(jsonPath("id").isNotEmpty())
+				.andExpect(jsonPath("title").value("Meu Livro")).andExpect(jsonPath("autor").value("Autor"))
+				.andExpect(jsonPath("isbn").value("123321"));
+
+	}
+
+	@Test
+	@DisplayName("Deve lançar erro de validação quando não houver dados suficientes para criação do livro.")
+	public void createInvalidBookTest() {
+
+	}
+}
