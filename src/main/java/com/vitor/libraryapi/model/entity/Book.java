ae39b12eca23814dacda3d161eaf1c7c908fb904
vitor7001 <vitorgomes7001@gmail.com>
2020-04-12 19:46:03 -0300

    Mapeamento da entidade Book com anotações JPA

--- src/main/java/com/vitor/libraryapi/model/entity/Book.java
+++ src/main/java/com/vitor/libraryapi/model/entity/Book.java
@@ -1,5 +1,12 @@
 package com.vitor.libraryapi.model.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,11 +16,22 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table
 public class Book {
 
+	@Id
+	@Column
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@Column
 	private String title;
+
+	@Column
 	private String autor;
+
+	@Column
 	private String isbn;
 
 }
