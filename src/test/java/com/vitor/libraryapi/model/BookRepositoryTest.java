6fa38abffad3992d67a67f239016e0a81c640cbc
vitor7001 <vitorgomes7001@gmail.com>
2020-04-16 19:20:18 -0300

    Teste no repository onde deve salvar e teste onde deve deletar

--- src/test/java/com/vitor/libraryapi/model/BookRepositoryTest.java
+++ src/test/java/com/vitor/libraryapi/model/BookRepositoryTest.java
@@ -45,10 +45,6 @@ public class BookRepositoryTest {
 
 	}
 
-	private Book createBook(String isbn) {
-		return Book.builder().title("Aventuras").autor("Joao").isbn(isbn).build();
-	}
-
 	@Test
 	@DisplayName("Deve retornar falso quando não existir um livro na base com o isbn informado.")
 	public void returnFalseWhehIsbnDoesntExists() {
@@ -87,6 +83,38 @@ public class BookRepositoryTest {
 
 		assertThat(repository.findById(book.getId())).isEmpty();
 
+	}
+
+	@Test
+	@DisplayName("Deve salvar um livro.")
+	public void saveBookTest() {
+
+		Book book = createBook("12");
+
+		Book savedBook = repository.save(book);
+
+		assertThat(savedBook.getId()).isNotNull();
+	}
+
+	@Test
+	@DisplayName("Deve deletar um livro.")
+	public void deleteBookTest() {
+
+		Book book = createBook("12");
+
+		tem.persist(book);
+
+		Book foundBook = tem.find(Book.class, book.getId());
+
+		repository.delete(foundBook);
+
+		Book deletedBook = tem.find(Book.class, book.getId());
+
+		assertThat(deletedBook).isNull();
+	}
+
+	private Book createBook(String isbn) {
+		return Book.builder().title("Aventuras").autor("Joao").isbn(isbn).build();
 	}
 
 }
